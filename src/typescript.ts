ae0a632eb8398f12d8796f3f5e7596932063b992
jamiben <jamilazerara@yahoo.fr>
2022-12-03 20:54:46 +0100

    seventh commit if string value error

--- src/typescript.ts
+++ src/typescript.ts
@@ -6,9 +6,9 @@ const EUR = 1.04;
 
 const select1 = (document.querySelector("#choice1") as HTMLSelectElement);
 const select2 = (document.querySelector("#choice2") as HTMLSelectElement);
-const symbol1 = (document.querySelector("#symbol1") as HTMLElement );
+const flag1 = (document.querySelector("#symbol1") as HTMLSpanElement);
+const flag2 = (document.querySelector("#symbol2") as HTMLSpanElement);
 const symbol2 = (document.querySelector("#symbol2") as HTMLElement);
-const input1 = (document.querySelector("#input1") as HTMLInputElement);
 let input2 = (document.querySelector("#input2") as HTMLInputElement);
 const submit = (document.querySelector("#submit") as HTMLButtonElement);
 const startValue = (document.querySelector("#startValue") as HTMLOptionElement);
@@ -16,16 +16,76 @@ const compar = (document.querySelector("#compar") as HTMLOptionElement);
 
 let i = 0;
 let tabOptions = [
-    {name: "USD", rate:1.04, symbol:"$"},
-    {name:"IDR", rate:0.000064, symbol:"RP"},
-    {name:"DNZ", rate:0.0072, symbol:"DA"},
-    {name:"RB", rate:0.016, symbol:"₽"},
-    {name:"EUR", rate:1.04, symbol:"€"},
+    {name: "USD", rate:1.04, symbol: "🇺🇸"},
+    {name:"IDR", rate:0.000064, symbol:"🇮🇩"},
+    {name:"DNZ", rate:0.0072, symbol:"🇩🇿"},
+    {name:"RB", rate:0.016, symbol:"🇷🇺"},
+    {name:"EUR", rate:1.04, symbol:"🇪🇺"},
 ];
 
-let valeurSelect; 
-let tauxEnCours;
+let flagA = tabOptions[0].symbol;
+let flagB = tabOptions[0].symbol;
+flag1.innerHTML = `${flagA}`;
+flag2.innerHTML = `${flagB}`;
 
+
+select1.addEventListener('change', () => {
+
+    let select1Value = select1.selectedIndex;
+    // console.log(select1Value);
+
+    switch(select1Value){
+        case 0: 
+            flagA = tabOptions[0].symbol;
+            console.log(flagA);
+            break;
+        case 1: 
+            flagA = tabOptions[1].symbol;
+            console.log(flagA);
+            break;
+        case 2: 
+            flagA = tabOptions[2].symbol;
+            console.log(flagA);
+            break;
+        case 3: 
+            flagA = tabOptions[3].symbol;
+            console.log(flagA);
+            break;
+        case 4: 
+            flagA = tabOptions[4].symbol;
+            console.log(flagA);
+            break;
+    }   
+    flag1.innerHTML = `${flagA}`;
+});
+select2.addEventListener("change", () => {
+    let select2Value = select2.selectedIndex; 
+    // console.log(select2Value);
+
+    switch(select2Value){
+        case 0:
+            flagB = tabOptions[0].symbol;
+            console.log(flagB);
+            break;
+        case 1:
+            flagB = tabOptions[1].symbol;
+            console.log(flagB);
+            break;
+        case 2:
+            flagB = tabOptions[2].symbol;
+            console.log(flagB);
+            break;
+        case 3:
+            flagB = tabOptions[3].symbol;
+            console.log(flagB);
+            break;
+        case 4:
+            flagB = tabOptions[4].symbol;
+            console.log(flagB);
+            break;
+    }
+    flag2.textContent = `${flagB}`;
+});
 
 for( i=0; i <= tabOptions.length -1; i++ ){
     select1.options[i] = new Option(tabOptions[i].name);
@@ -34,10 +94,10 @@ for( i=0; i <= tabOptions.length -1; i++ ){
 
 submit.onclick = function submit() {
     event?.preventDefault(); 
+
+        // let flag: string = tabOptions[0].symbol;
         let error = (document.querySelector("#error")as HTMLSpanElement);
-        console.log(error)
         const input1 = (document.querySelector("#input1") as HTMLInputElement).value;
-        let symbol1 = (document.querySelector("#symbol1") as HTMLSpanElement).innerHTML;
         let result: number;
         const valueInp: number = parseFloat(input1);
         let rating1 = select1.value;
@@ -50,28 +110,31 @@ submit.onclick = function submit() {
                 
             case "USD": 
                 result = valueInp! * tabOptions[0].rate; 
-                symbol1 = tabOptions[0].symbol;
                 // alert("dollards");
-                // console.log(tabOptions[0].rate);
+                // console.log(tabOptions[0].symbol);
+                // flag = tabOptions[0].symbol;
                 break; 
             case "IDR": 
                 result = valueInp * tabOptions[1].rate;
                 console.log(result);
-                symbol1 = tabOptions[1].symbol;
+                // flag = tabOptions[1].symbol;
                 // alert("roupies");
                 break;
             case "DNZ":
                 result = valueInp * tabOptions[2].rate;
+                // flag = tabOptions[2].symbol;
                 // console.log(tabOptions[2].rate);
                 // alert("dinard algerien");
                 break;
             case "RB":
                 result = valueInp * tabOptions[3].rate;
+                // flag = tabOptions[3].symbol;
                 // console.log(tabOptions[3].rate);
                 // alert("roubles");
                 break;
             case "EUR":
                 result = valueInp * tabOptions[4].rate;
+                // flag = tabOptions[4].symbol;
                 // console.log(tabOptions[4].rate);
                 // alert("euro");
                 break;
@@ -108,11 +171,15 @@ submit.onclick = function submit() {
             error!.innerHTML = 'is not possible to converte with same rating, please select an other money';
         }
 
+        if(typeof input1.valueOf() == "string"){
+            error!.innerHTML = "give number";
+        }
             // console.log(target1.value);
             // console.log(result);       
             // console.log(target1.value);
             // console.log(result);          
             input2.value =  `${result!}`;
+            // flag1.innerHTML = `${flag}`;
 }
         
 
