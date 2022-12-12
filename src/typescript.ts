f8afcc32a7f7c0168fce82515476807c1c8dc543
jamiben <jamilazerara@yahoo.fr>
2022-12-12 14:51:19 +0100

    last commit javascript

--- src/typescript.ts
+++ src/typescript.ts
@@ -4,16 +4,13 @@ const DZ = 0.0072;
 const rouble = 0.016;
 const EUR = 1.04;
 
-const select1 = (document.querySelector("#choice1") as HTMLSelectElement);
+let select1 = (document.querySelector("#choice1") as HTMLSelectElement);
 const select2 = (document.querySelector("#choice2") as HTMLSelectElement);
 const flag1 = (document.querySelector("#symbol1") as HTMLSpanElement);
 const flag2 = (document.querySelector("#symbol2") as HTMLSpanElement);
-const symbol2 = (document.querySelector("#symbol2") as HTMLElement);
 const input1 = (document.querySelector("#input1") as HTMLInputElement);
 const input2 = (document.querySelector("#input2") as HTMLInputElement);
 const submit = (document.querySelector("#submit") as HTMLButtonElement);
-const startValue = (document.querySelector("#startValue") as HTMLOptionElement);
-const compar = (document.querySelector("#compar") as HTMLOptionElement);
 const swap = (document.querySelector('#swap')as HTMLElement);
 
 let i = 0;
@@ -25,6 +22,7 @@ let tabOptions = [
     {name:"EUR", rate:1.04, symbol:"🇪🇺"},
 ];
 
+
 let flagA = tabOptions[0].symbol;
 let flagB = tabOptions[0].symbol;
 flag1.innerHTML = `${flagA}`;
@@ -32,39 +30,31 @@ flag2.innerHTML = `${flagB}`;
 
 
 select1.addEventListener('change', () => {
-
     let select1Value = select1.selectedIndex;
-    let select2Value = select2.selectedIndex; 
 
-    // console.log(select1Value);
 
-    switch(select1Value && select2Value){
+    switch(select1Value ){
         case 0: 
             flagA = tabOptions[0].symbol;
-            // console.log(flagA);
             break;
         case 1: 
             flagA = tabOptions[1].symbol;
-            // console.log(flagA);
             break;
         case 2: 
             flagA = tabOptions[2].symbol;
-            // console.log(flagA);
             break;
         case 3: 
             flagA = tabOptions[3].symbol;
-            // console.log(flagA);
             break;
         case 4: 
             flagA = tabOptions[4].symbol;
-            // console.log(flagA);
             break;
     }   
     flag1.innerHTML = `${flagA}`;
 });
+
 select2.addEventListener("change", () => {
     let select2Value = select2.selectedIndex; 
-    // console.log(select2Value);
 
     switch(select2Value){
         case 0:
@@ -72,19 +62,15 @@ select2.addEventListener("change", () => {
             break;
         case 1:
             flagB = tabOptions[1].symbol;
-            // console.log(flagB);
             break;
         case 2:
             flagB = tabOptions[2].symbol;
-            // console.log(flagB);
             break;
         case 3:
             flagB = tabOptions[3].symbol;
-            // console.log(flagB);
             break;
         case 4:
             flagB = tabOptions[4].symbol;
-            // console.log(flagB);
             break;
     }
     flag2.textContent = `${flagB}`;
@@ -111,28 +97,18 @@ submit.onclick = function Submit(event) {
         switch(rating1){      
             case "USD": 
                 result = valueInp! * tabOptions[0].rate; 
-                // console.log(tabOptions[0].symbol);
-                // flag = tabOptions[0].symbol;
                 break; 
             case "IDR": 
                 result = valueInp * tabOptions[1].rate;
-                console.log(result);
-                // flag = tabOptions[1].symbol;
                 break;
             case "DNZ":
                 result = valueInp * tabOptions[2].rate;
-                // flag = tabOptions[2].symbol;
-                // console.log(tabOptions[2].rate);
                 break;
             case "RB":
                 result = valueInp * tabOptions[3].rate;
-                // flag = tabOptions[3].symbol;
-                // console.log(tabOptions[3].rate);
                 break;
             case "EUR":
                 result = valueInp * tabOptions[4].rate;
-                // flag = tabOptions[4].symbol;
-                // console.log(tabOptions[4].rate);
                 break;
         }  
         
@@ -140,28 +116,25 @@ submit.onclick = function Submit(event) {
                 
             case "USD": 
                 result = valueInp! / tabOptions[0].rate; 
-                // alert("dollards");
                 break; 
             case "IDR": 
                 result = valueInp / tabOptions[1].rate;
-                // alert("roupies");
                 break;
             case "DNZ":
                 result = valueInp / tabOptions[2].rate;
-                // alert("dinard algerien");
                 break;
             case "RB":
                 result = valueInp / tabOptions[3].rate;
-                // alert("roubles");
                 break;
             case "EUR":
                 result = valueInp / tabOptions[4].rate;
-                // alert("euro");
                 break;         
         };
 
         error.innerHTML = "";
-        const numberValue = (input1).valueAsNumber;
+        const numberValue = (input1).valueAsNumber;  
+
+    
 
         if (rating1 === rating2){
             error!.innerHTML = 'is not possible to converte with same rating, please select an other money';
@@ -172,9 +145,9 @@ submit.onclick = function Submit(event) {
         else{
             input2.value =  `${result!}`;
         }// console.log(isNaN(numberValue));
-
-        // swatchInput.addEventListener
 }
+
+
 swap.onclick = function swatchInput(event) {
     input1.value = "";
     event?.preventDefault(); 
@@ -185,7 +158,6 @@ swap.onclick = function swatchInput(event) {
 
     value1 = result2;
     result2 = swapAction;
-
     input1.value = value1;
     (document.querySelector("#input2")as HTMLInputElement).value = result2; 
 }
